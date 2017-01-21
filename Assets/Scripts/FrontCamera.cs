b3ae1f81b6fe062b07e597dfb1bc33046bdbbef8
kuribohlv9 <kuribohlv9@gmail.com>
2017-01-21 19:52:45 +0100

    camera speed up and other stuff

---
+++ Assets/Scripts/FrontCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontCamera : MonoBehaviour {
+
+    public CameraMove camera;
+    private float originalspeed;
+
+	// Use this for initialization
+	void Start () {
+        originalspeed = camera.speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            camera.speed = originalspeed * 3;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            camera.speed = originalspeed;
+        }
+    }
+}
